f0b119283e22a36438e6da5c8772ceac5fbd3621
davidoskiii <davide.mascherin9@gmail.com>
2024-02-24 14:16:07 +0100

    Added an hash map to the language so that i can add variables

--- vm/vm.h
+++ vm/vm.h
@@ -2,6 +2,7 @@
 #define cluminique_vm_h
 
 #include "../chunk/chunk.h"
+#include "../table/table.h"
 #include "../value/value.h"
 
 #define STACK_MAX 256
@@ -11,6 +12,7 @@ typedef struct {
   uint8_t* ip;
   Value stack[STACK_MAX];
   Value* stackTop;
+  Table strings;
   Obj* objects;
 } VM;
 
