93ee89782e29376f4f39e0d5c8603f179832366d
davidoskiii <davide.mascherin9@gmail.com>
2024-03-28 10:56:23 +0100

    Added scanner support for TOKEN_PLUS_PLUS (++) and TOKEN_MINUS_MINUS (--)ù

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -264,8 +264,10 @@ Token scanToken() {
       
       return makeToken(TOKEN_DOT);
     }
-    case '-': return makeToken(TOKEN_MINUS);
-    case '+': return makeToken(TOKEN_PLUS);
+    case '-': return makeToken(
+          match('-') ? TOKEN_MINUS_MINUS : TOKEN_MINUS);
+    case '+': return makeToken(
+          match('+') ? TOKEN_PLUS_PLUS : TOKEN_PLUS);
     case '/': return makeToken(TOKEN_SLASH);
     case '*': return makeToken(TOKEN_STAR); 
     case '%': return makeToken(TOKEN_MODULO);
