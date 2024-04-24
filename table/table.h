ac8097e715af3120056ae599a7d4a5e33fd56980
davidoskiii <davide.mascherin9@gmail.com>
2024-04-24 16:12:43 +0200

    Minor change

--- table/table.h
+++ table/table.h
@@ -25,6 +25,7 @@ bool tableSet(Table* table, ObjString* key, Value value);
 bool tableDelete(Table* table, ObjString* key);
 void tableAddAll(Table* from, Table* to);
 ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
+void printTable(Table table);
 void tableRemoveWhite(Table* table);
 void markTable(Table* table);
 
