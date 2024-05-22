d609144a6735899c305f846c571871095f3afc0a
davidoskiii <davide.mascherin9@gmail.com>
2024-05-22 21:19:09 +0200

    Added more url methods

--- network/network.c
+++ network/network.c
@@ -10,6 +10,30 @@
 #include "../value/value.h"
 #include "../vm/vm.h"
 
+static bool urlIsAbsolute(ObjInstance* url) {
+  ObjString* host = AS_STRING(getObjProperty(url, "host"));
+  return host->length > 0;
+}
+
+static ObjString* urlToString(ObjInstance* url) {
+  ObjString* scheme = AS_STRING(getObjProperty(url, "scheme"));
+  ObjString* host = AS_STRING(getObjProperty(url, "host"));
+  int port = AS_INT(getObjProperty(url, "port"));
+  ObjString* path = AS_STRING(getObjProperty(url, "path"));
+  ObjString* query = AS_STRING(getObjProperty(url, "query"));
+  ObjString* fragment = AS_STRING(getObjProperty(url, "fragment"));
+
+  ObjString* urlString = newString("");
+  if (host->length > 0) {
+    urlString = (scheme->length > 0) ? formattedString("%s://%s", scheme->chars, host->chars) : host;
+    if (port > 0 && port < 65536) urlString = formattedString("%s:%d", urlString->chars, port);
+  }
+  if (path->length > 0) urlString = formattedString("%s/%s", urlString->chars, path->chars);
+  if (query->length > 0) urlString = formattedString("%s&%s", urlString->chars, query->chars);
+  if (fragment->length > 0) urlString = formattedString("%s#%s", urlString->chars, fragment->chars);
+  return urlString;
+}
+
 NATIVE_METHOD(URL, __init__) {
   assertArgCount("URL::__init__(scheme, host, port, path, query, fragment)", 6, argCount);
   assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 0);
@@ -26,21 +50,18 @@ NATIVE_METHOD(URL, __init__) {
   setObjProperty(self, "path", args[3]);
   setObjProperty(self, "query", args[4]);
   setObjProperty(self, "fragment", args[5]);
-  RETURN_OBJ(receiver);
+  setObjProperty(self, "raw", OBJ_VAL(urlToString(self)));
+  RETURN_OBJ(self);
 }
 
 NATIVE_METHOD(URL, isAbsolute) {
   assertArgCount("URL::isAbsolute()", 0, argCount);
-  ObjInstance* self = AS_INSTANCE(receiver);
-  ObjString* host = AS_STRING(getObjProperty(self, "host"));
-  RETURN_BOOL(host->length > 0);
+  RETURN_BOOL(urlIsAbsolute(AS_INSTANCE(receiver)));
 }
 
 NATIVE_METHOD(URL, isRelative) {
   assertArgCount("URL::isRelative()", 0, argCount);
-  ObjInstance* self = AS_INSTANCE(receiver);
-  ObjString* host = AS_STRING(getObjProperty(self, "host"));
-  RETURN_BOOL(host->length == 0);
+  RETURN_BOOL(!urlIsAbsolute(AS_INSTANCE(receiver)));
 }
 
 NATIVE_METHOD(URL, pathArray) {
@@ -92,33 +113,49 @@ NATIVE_METHOD(URL, queryDict) {
   }
 }
 
+NATIVE_METHOD(URL, relativize) {
+  assertArgCount("URL::relativize(url)", 1, argCount);
+  assertObjInstanceOfClass("URL::relativize(url)", args[0], "luminique::std::network", "URL", 0);
+  ObjInstance* self = AS_INSTANCE(receiver);
+  ObjInstance* url = AS_INSTANCE(args[0]);
+  if (urlIsAbsolute(self) || urlIsAbsolute(url)) RETURN_OBJ(url);
+
+  ObjString* urlString = AS_STRING(getObjProperty(self, "raw"));
+  ObjString* urlString2 = urlToString(url);
+  int index = searchString(urlString, urlString2, 0);
+  if (index == 0) {
+    ObjInstance* relativized = newInstance(self->obj.klass);
+    ObjString* relativizedURL = subString(urlString, urlString2->length, urlString->length); 
+    struct yuarel component;
+    char fullURL[UINT8_MAX];
+    int length = snprintf(fullURL, UINT8_MAX, "%s%s", "https://example.com/", relativizedURL->chars);
+    yuarel_parse(&component, fullURL);
+
+    setObjProperty(relativized, "scheme", OBJ_VAL(emptyString()));
+    setObjProperty(relativized, "host", OBJ_VAL(emptyString()));
+    setObjProperty(relativized, "port", INT_VAL(0));
+    setObjProperty(relativized, "path", OBJ_VAL(newString(component.path != NULL ? component.path : "")));
+    setObjProperty(relativized, "query", OBJ_VAL(newString(component.query != NULL ? component.query : "")));
+    setObjProperty(relativized, "fragment", OBJ_VAL(newString(component.fragment != NULL ? component.fragment : "")));
+    setObjProperty(relativized, "raw", OBJ_VAL(relativizedURL));
+    RETURN_OBJ(relativized);
+  }
+  RETURN_OBJ(url);
+}
+
 NATIVE_METHOD(URL, __str__) {
   assertArgCount("URL::__str__()", 0, argCount);
   ObjInstance* self = AS_INSTANCE(receiver);
-  ObjString* scheme = AS_STRING(getObjProperty(self, "scheme"));
-  ObjString* host = AS_STRING(getObjProperty(self, "host"));
-  int port = AS_INT(getObjProperty(self, "port"));
-  ObjString* path = AS_STRING(getObjProperty(self, "path"));
-  ObjString* query = AS_STRING(getObjProperty(self, "query"));
-  ObjString* fragment = AS_STRING(getObjProperty(self, "fragment"));
-
-  ObjString* uriString = newString("");
-  if (host->length > 0) {
-    uriString = (scheme->length > 0) ? formattedString("%s://%s", scheme->chars, host->chars) : host;
-    if (port > 0 && port < 65536) uriString = formattedString("%s:%d", uriString->chars, port);
-  }
-  if (path->length > 0) uriString = formattedString("%s/%s", uriString->chars, path->chars);
-  if (query->length > 0) uriString = formattedString("%s&%s", uriString->chars, query->chars);
-  if (fragment->length > 0) uriString = formattedString("%s#%s", uriString->chars, fragment->chars);
-  RETURN_OBJ(uriString);
+  ObjString* raw = AS_STRING(getObjProperty(self, "raw"));
+  RETURN_OBJ(raw);
 }
-
 
 NATIVE_METHOD(URLClass, parse) {
   assertArgCount("URL class::parse(url)", 1, argCount);
   assertArgIsString("URL class::parse(url)", args, 0);
   ObjInstance* instance = newInstance(AS_CLASS(receiver));
   ObjString* url = AS_STRING(args[0]);
+
   struct yuarel component;
   if (yuarel_parse(&component, url->chars) == -1) {
     runtimeError("Failed to parse url.");
@@ -131,6 +168,7 @@ NATIVE_METHOD(URLClass, parse) {
   setObjProperty(instance, "path", OBJ_VAL(newString(component.path != NULL ? component.path : "")));
   setObjProperty(instance, "query", OBJ_VAL(newString(component.query != NULL ? component.query : "")));
   setObjProperty(instance, "fragment", OBJ_VAL(newString(component.fragment != NULL ? component.fragment : "")));
+  setObjProperty(instance, "raw", OBJ_VAL(urlToString(instance)));
   RETURN_OBJ(instance);
 }
 
@@ -146,6 +184,7 @@ void registerNetworkPackage() {
   DEF_METHOD(urlClass, URL, isRelative, 0);
   DEF_METHOD(urlClass, URL, pathArray, 0);
   DEF_METHOD(urlClass, URL, queryDict, 0);
+  DEF_METHOD(urlClass, URL, relativize, 1);
 
   ObjClass* urlMetaclass = urlClass->obj.klass;
   DEF_METHOD(urlMetaclass, URLClass, parse, 1);
