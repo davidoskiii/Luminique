259763497da4349819732ff7ebfac8c0090a9a1b
davidoskiii <davide.mascherin9@gmail.com>
2024-05-22 19:09:29 +0200

    Added url class

--- network/network.c
+++ network/network.c
@@ -8,14 +8,14 @@
 #include "../value/value.h"
 #include "../vm/vm.h"
 
-NATIVE_METHOD(URI, __init__) {
-  assertArgCount("URI::__init__(scheme, host, port, path, query, fragment)", 6, argCount);
-  assertArgIsString("URI::__init__(scheme, host, port, path, query, fragment)", args, 0);
-  assertArgIsString("URI::__init__(scheme, host, port, path, query, fragment)", args, 1);
-  assertArgIsInt("URI::__init__(scheme, host, port, path, query, fragment)", args, 2);
-  assertArgIsString("URI::__init__(scheme, host, port, path, query, fragment)", args, 3);
-  assertArgIsString("URI::__init__(scheme, host, port, path, query, fragment)", args, 4);
-  assertArgIsString("URI::__init__(scheme, host, port, path, query, fragment)", args, 5);
+NATIVE_METHOD(URL, __init__) {
+  assertArgCount("URL::__init__(scheme, host, port, path, query, fragment)", 6, argCount);
+  assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 0);
+  assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 1);
+  assertArgIsInt("URL::__init__(scheme, host, port, path, query, fragment)", args, 2);
+  assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 3);
+  assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 4);
+  assertArgIsString("URL::__init__(scheme, host, port, path, query, fragment)", args, 5);
 
   ObjInstance* self = AS_INSTANCE(receiver);
   setObjProperty(self, "scheme", args[0]);
@@ -27,8 +27,22 @@ NATIVE_METHOD(URI, __init__) {
   RETURN_OBJ(receiver);
 }
 
-NATIVE_METHOD(URI, __str__) {
-  assertArgCount("URI::__str__()", 0, argCount);
+NATIVE_METHOD(URL, isAbsolute) {
+  assertArgCount("URL::isAbsolute()", 0, argCount);
+  ObjInstance* self = AS_INSTANCE(receiver);
+  ObjString* host = AS_STRING(getObjProperty(self, "host"));
+  RETURN_BOOL(host->length > 0);
+}
+
+NATIVE_METHOD(URL, isRelative) {
+  assertArgCount("URL::isRelative()", 0, argCount);
+  ObjInstance* self = AS_INSTANCE(receiver);
+  ObjString* host = AS_STRING(getObjProperty(self, "host"));
+  RETURN_BOOL(host->length == 0);
+}
+
+NATIVE_METHOD(URL, __str__) {
+  assertArgCount("URL::__str__()", 0, argCount);
   ObjInstance* self = AS_INSTANCE(receiver);
   ObjString* scheme = AS_STRING(getObjProperty(self, "scheme"));
   ObjString* host = AS_STRING(getObjProperty(self, "host"));
@@ -37,8 +51,11 @@ NATIVE_METHOD(URI, __str__) {
   ObjString* query = AS_STRING(getObjProperty(self, "query"));
   ObjString* fragment = AS_STRING(getObjProperty(self, "fragment"));
 
-  ObjString* uriString = formattedString("%s://%s", scheme->chars, host->chars);
-  if (port > 0 && port < 65536) uriString = formattedString("%s:%d", uriString->chars, port);
+  ObjString* uriString = newString("");
+  if (host->length > 0) {
+    uriString = (scheme->length > 0) ? formattedString("%s://%s", scheme->chars, host->chars) : host;
+    if (port > 0 && port < 65536) uriString = formattedString("%s:%d", uriString->chars, port);
+  }
   if (path->length > 0) uriString = formattedString("%s/%s", uriString->chars, path->chars);
   if (query->length > 0) uriString = formattedString("%s&%s", uriString->chars, query->chars);
   if (fragment->length > 0) uriString = formattedString("%s#%s", uriString->chars, fragment->chars);
@@ -49,10 +66,12 @@ void registerNetworkPackage() {
   ObjNamespace* networkNamespace = defineNativeNamespace("network", vm.stdNamespace);
   vm.currentNamespace = networkNamespace;
 
-  ObjClass* uriClass = defineNativeClass("URI");
-  bindSuperclass(uriClass, vm.objectClass);
-  DEF_METHOD(uriClass, URI, __init__, 6);
-  DEF_METHOD(uriClass, URI, __str__, 0);
+  ObjClass* urlClass = defineNativeClass("URL");
+  bindSuperclass(urlClass, vm.objectClass);
+  DEF_METHOD(urlClass, URL, __init__, 6);
+  DEF_METHOD(urlClass, URL, __str__, 0);
+  DEF_METHOD(urlClass, URL, isAbsolute, 0);
+  DEF_METHOD(urlClass, URL, isRelative, 0);
 
   vm.currentNamespace = vm.rootNamespace;
 }
